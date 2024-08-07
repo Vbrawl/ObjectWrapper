50cc2851342f50d8f043ca91f49061f57670a6c5
VBrawl <konstantosjim@gmail.com>
2024-08-07 16:33:09 +0300

    Added unwrap for all ctypes

--- includes/ctypes.h
+++ includes/ctypes.h
@@ -7,16 +7,19 @@
 /* Integer */
 typedef OWObject_t OWO_Integer_t;
 OWO_Integer_t* OWInteger_Construct(int object);
+#define OWInteger_UnWrap(this) (*(int*)this->object)
 
 
 /* Character */
 typedef OWObject_t OWO_Character_t;
 OWO_Character_t* OWCharacter_Construct(char object);
+#define OWCharacter_UnWrap(this) (*(char*)this->object)
 
 
 /* Boolean */
 typedef OWObject_t OWO_Boolean_t;
 OWO_Boolean_t* OWBoolean_Construct(bool object);
+#define OWBoolean_UnWrap(this) (*(bool*)this->object)
 
 
 
--- tests/ctypes.c
+++ tests/ctypes.c
@@ -5,15 +5,15 @@
 int main() {
 
   OWO_Integer_t* number = OWInteger_Construct(5);
-  if(*((int*)number->object) != 5) return -1;
+  if(OWInteger_UnWrap(number) != 5) return -1;
   OWObject_UnRef(number);
 
   OWO_Character_t* character = OWCharacter_Construct('a');
-  if(*((char*)character->object) != 'a') return -2;
+  if(OWCharacter_UnWrap(character) != 'a') return -2;
   OWObject_UnRef(character);
 
   OWO_Boolean_t* boolean = OWCharacter_Construct(true);
-  if(*((bool*)boolean->object) != true) return -3;
+  if(OWBoolean_UnWrap(boolean) != true) return -3;
   OWObject_UnRef(boolean);
 
   return 0;
