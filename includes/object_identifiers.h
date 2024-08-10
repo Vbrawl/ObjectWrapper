4adcb315fd1510c587dc899d150b3ad0502812cf
VBrawl <konstantosjim@gmail.com>
2024-08-11 01:24:56 +0300

    Added documentation for object_identifiers.h

--- includes/object_identifiers.h
+++ includes/object_identifiers.h
@@ -1,7 +1,13 @@
 #ifndef OW_OBJECT_IDENTIFIERS_H
 #define OW_OBJECT_IDENTIFIERS_H
 
-// Type for OWID_* constants
+/**
+ * @brief Object Wrapper Identifier
+ *
+ * This is the type for the object identifiers
+ *
+ * All values for this type should have the `OWID_` prefix.
+ */
 typedef int OWID;
 enum {
   OWID_UNDEFINED = 0,
